59005fcd3317c0d5a20311caaac5700a9d19a1b8
Akash-ridescan <akash@ridescan.ai>
2025-03-26 21:52:18 +0530

    height issue fixed info page

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-      >
+      <body>
          <AuthContextProvider>
           {children}
         </AuthContextProvider>
